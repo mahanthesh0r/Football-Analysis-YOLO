0f7493e54db3e48488081f13b186df8ea360aa2d
Mahanthesh <monty.officio@gmail.com>
2024-05-01 10:10:40 -0400

    added player to ball tracker

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from utils import read_video, save_video
 from trackers import Tracker
 import cv2
 from team_assigner import TeamAssigner
+from player_ball_assigner import PlayerBallAssigner
 
 def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
@@ -34,6 +35,15 @@ def main():
             team = team_assigner.get_player_team(video_frames[frame_num], track['bbox'], player_id)
             tracks['players'][frame_num][player_id]['team'] = team
             tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+    #ASSIGN BALL TO PLAYER
+    player_ball_assigner = PlayerBallAssigner()
+    for frame_num, player_track in enumerate(tracks['players']):
+        ball_bbox = tracks['ball'][frame_num][1]['bbox']
+        assigned_player = player_ball_assigner.assign_ball_to_player(player_track, ball_bbox)
+
+        if assigned_player != -1:
+            tracks['players'][frame_num][assigned_player]['has_ball'] = True
 
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
     
--- trackers/tracker.py
+++ trackers/tracker.py
@@ -150,6 +150,9 @@ class Tracker:
                 color = player.get("team_color", (0,0,255))
                 frame = self.draw_ellipse(frame, player["bbox"], color, track_id)
 
+                if player.get("has_ball", False):
+                    frame = self.draw_triangle(frame, player["bbox"], (0,0,255))
+
 
             #Draw Referees 
             for _, referee in referee_dict.items():
