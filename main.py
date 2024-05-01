fb9f66ed32110d9f06c5d8f4bbfb448b3b0d834a
Mahanthesh <monty.officio@gmail.com>
2024-04-30 20:48:42 -0400

    added player colors to annotation

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from utils import read_video, save_video
 from trackers import Tracker
+import cv2
 
 def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
 
+
+
     tracker = Tracker('models/best.pt')
 
     tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pk1')
+
+    #CROP PLAYER IMAGE
+    # for track_id, player in tracks['players'][0].items():
+    #     bbox = player['bbox']
+    #     frame = video_frames[0]
+
+    #     cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+
+    #     cv2.imwrite(f'output_videos/cropped_image.jpg', cropped_image)
+    #     break
+        
 
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
     
